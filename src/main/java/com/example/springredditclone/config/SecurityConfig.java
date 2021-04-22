3661c1b81623774f527454b626adadee33ef0ed0
Jacek <jaceksysiak@wp.pl>
2021-04-22 06:49:20 +0200

    22-04-2021 06:49

--- src/main/java/com/example/springredditclone/config/SecurityConfig.java
+++ src/main/java/com/example/springredditclone/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springredditclone.config;
 
+
 import com.example.springredditclone.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
